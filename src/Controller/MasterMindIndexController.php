a9153d9fe7fbeb453556c4cfabf1c8158cab13f3
David Sanchez <david85trabajo@gmail.com>
2018-02-18 10:47:49 +0100

    controlador para hacer las jugadas

--- src/Controller/MasterMindIndexController.php
+++ src/Controller/MasterMindIndexController.php
@@ -2,7 +2,6 @@
 // src/Controller/HelloWorldController.php
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\MasterMindGame;
@@ -23,13 +22,4 @@ class MasterMindIndexController extends Controller
         ));
     }
 
-    /**
-      * @Route("/games/{gameid}", name="games")
-      */
-      public function obtainGameDetails($gameid)
-      {
-            return new Response(
-                '<html><body>Buscar el juego: '.$gameid.'</body></html>'
-            );
-      }
 }

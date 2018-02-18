ef8644a895d9734054306964c3b4af123e63bd47
David Sanchez <david85trabajo@gmail.com>
2018-02-18 10:14:32 +0100

    formulario para dar nombre a la partida

--- src/Controller/NewGameController.php
+++ src/Controller/NewGameController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\MasterMindGame;
 use App\Entity\State;
 
@@ -13,37 +16,55 @@ class NewGameController extends Controller
     /**
       * @Route("/new/", name="new")
       */
-    public function start()
+    public function start(Request $request)
     {
         $newGame = new MasterMindGame();
-        $newGame->setName('MMGame1');
-        $newGame->setCreationDate(new \DateTime());
-        $newGame->setColorList(
-            array(
-                rand(0,8),
-                rand(0,8),
-                rand(0,8),
-                rand(0,8)
-            )
-        );
-        $newGame->setState(State::STARTED);
+        $form = $this->createFormBuilder($newGame)
+            ->add('name', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Actualizar nombre'))
+            ->getForm();
 
-        //obtenemos el acceso a la BD
-        $em = $this->getDoctrine()->getManager();
+        $form->handleRequest($request);
 
-        // guardar en BD
-        $em->persist($newGame);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //recuperamos el nombre del formulario
+            $newGame = $form->getData();
 
-        // ejecutar (realmente) la query
-        $em->flush();
+            if((null == $newGame->getName()) || trim($newGame->getName())===''){
+                $newGame->setName('MasterMindGame');
+            };
+            $newGame->setCreationDate(new \DateTime());
+            $newGame->setColorList(
+                array(
+                    rand(0,8),
+                    rand(0,8),
+                    rand(0,8),
+                    rand(0,8)
+                )
+            );
+            $newGame->setState(State::STARTED);
+    
+            //obtenemos el acceso a la BD
+            $em = $this->getDoctrine()->getManager();
+    
+            // guardar en BD
+            $em->persist($newGame);
+    
+            // ejecutar (realmente) la query
+            $em->flush();  
+            
+            return $this->render('games/game.html.twig', array(
+                'id' => $newGame->getId(),
+                'creationDate' => $newGame->getCreationDate()->format('Y-m-d H:i:s'),
+                'state' => $newGame->getState(),
+                'name' => $newGame->getName(),
+            ));
+        }else{
+            return $this->render('games/newgame.html.twig', array(
+                'form' => $form->createView(),
+            )); 
+        }
 
-        return $this->render('games/game.html.twig', array(
-            'id' => $newGame->getId(),
-            'creationDate' => $newGame->getCreationDate()->format('Y-m-d H:i:s'),
-            'state' => $newGame->getState(),
-            'color' => MasterMindGame::DEFINED_COLORS[
-                $newGame->getColorList()[0]
-                ]
-        ));
+      
     }
 }

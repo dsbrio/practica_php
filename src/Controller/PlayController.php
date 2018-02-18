eeb28afcf66ee3a1a084956932e165acd044bd56
cparandojo <carlosperezarandojo@gmail.com>
2018-02-18 14:07:36 +0100

    Validación inicial, ver el estado de la partida seleccionada, si es victoria o derrota se muestra pagina de error indicando que seleccione otra partida o cree una nueva

--- src/Controller/PlayController.php
+++ src/Controller/PlayController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\MasterMindGame;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\State;
 
 class PlayController extends Controller
 {
@@ -20,8 +21,27 @@ class PlayController extends Controller
             ->getRepository(MasterMindGame::class)
             ->find($gameid);
 
-            return $this->render('games/play.html.twig', array(
-                'id' => $game->getName(),
-            ));
+
+            if(null!==$game && State::STARTED===$game->getState()){
+
+                //La partida no ha finalizado, puede continuarla.
+                return $this->render('games/play.html.twig', array(
+                    'name' => $game->getName(),
+                ));
+            }else if(null!==$game && State::STARTED!==$game->getState()){
+
+                $state = $game->getState();
+
+                return $this->render('error/error.html.twig', array(
+                    'error' => "La partida ha finalizado con resultado: $state . Seleccione otra partida o cree una nueva.",
+                ));
+            }else{
+                return $this->render('error/error.html.twig', array(
+                    'error' => 'Error en la selección de la partida, por favor, inicie una nueva.',
+                ));
+            }
+
+
+
       }
 }

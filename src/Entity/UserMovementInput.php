fa8bd76c4e670d4335db8688228587efaf1dcf07
David Sanchez <david85trabajo@gmail.com>
2018-02-19 20:02:39 +0100

    mensaje de error cuando falla la validación

--- src/Entity/UserMovementInput.php
+++ src/Entity/UserMovementInput.php
@@ -6,7 +6,10 @@ use Symfony\Component\Validator\Constraints as Assert;
 class UserMovementInput
 {
     /**
-     * @Assert\Regex("/^[0-9]{6}$/")
+     * @Assert\Regex(
+     * pattern="/^[0-9]{6}$/",
+     * message="Debes escribir 6 números entre el 0 y el 9"
+     * )
      */
     public  $inputString; //validación del input del usuario con esta expresión regular
 
